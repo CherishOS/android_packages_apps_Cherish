0adabffe16986b9da20463a31aa79ffb5a4f68b9
beanstown106 <nbenis106@gmail.com>
2017-10-14 23:17:23 +0530

    Cherish: incall vibration options [3/3]
    *allow setting vibration when call is connected
    *allow setting vibration when call is disconnected
    *allow setting vibration for call waiting
    *this works with google and aosp dialer :)
    
    Change-Id: Id1a431124174571f8d0091825af2418971cfe35d
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/cherish/settings/fragments/NotificationSettings.java
+++ src/com/cherish/settings/fragments/NotificationSettings.java
@@ -31,12 +31,13 @@ import com.cherish.settings.preferences.SystemSettingListPreference;
 import com.cherish.settings.preferences.SystemSettingSwitchPreference;
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 import com.android.internal.util.cherish.CherishUtils;
-
 import java.util.ArrayList;
 import java.util.List;
 
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
 public class NotificationSettings extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener{
+
+    private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -46,6 +47,11 @@ public class NotificationSettings extends SettingsPreferenceFragment implements 
         ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
         final Resources res = getResources();
+
+        PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS);
+        if (!CherishUtils.isVoiceCapable(getActivity())) {
+                prefScreen.removePreference(incallVibCategory);
+        }
         
     }
 
