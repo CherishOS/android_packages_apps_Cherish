ee150661b7f860f5dbdcecdf9e4f9a5170aa2f6d
Joe Maples <joe@frap129.org>
2018-01-22 21:19:34 -0500

    Cherish: Smart Pixels [2/2]
    Change-Id: I55775e02e849ad5db5b1187be0ca77b7c09f1081
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/cherish/settings/fragments/MiscSettings.java
+++ src/com/cherish/settings/fragments/MiscSettings.java
@@ -34,13 +34,27 @@ import com.cherish.settings.preferences.SystemSettingListPreference;
 public class MiscSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+    private static final String SMART_PIXELS = "smart_pixels";
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         ContentResolver resolver = getActivity().getContentResolver();
 
         addPreferencesFromResource(R.xml.cherish_settings_misc);
+        updateSmartPixelsPreference();
 
+    }
+
+    private void updateSmartPixelsPreference() {
+        PreferenceScreen prefSet = getPreferenceScreen();
+        boolean enableSmartPixels = getContext().getResources().
+                getBoolean(com.android.internal.R.bool.config_enableSmartPixels);
+        Preference smartPixels = findPreference(SMART_PIXELS);
+
+        if (!enableSmartPixels){
+            prefSet.removePreference(smartPixels);
+        }
     }
 
     @Override
