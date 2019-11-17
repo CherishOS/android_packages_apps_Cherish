6ad6b05a6cf81071108e464ba074b6bb9e581d9e
Pranav Vashi <neobuddy89@gmail.com>
2019-11-17 22:47:35 +0530

    Cherish: Bring back Gaming mode [3/3]
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I4ffb0bdf8453068eb049da4823fea6da33346446

--- src/com/cherish/settings/fragments/MiscSettings.java
+++ src/com/cherish/settings/fragments/MiscSettings.java
@@ -2,44 +2,65 @@ package com.cherish.settings.fragments;
 
 import com.android.internal.logging.nano.MetricsProto;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
-import android.content.Context;
-import android.content.ContentResolver;
-import android.content.DialogInterface;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.os.SystemProperties;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.UserHandle;
+import android.content.ContentResolver;
+import android.content.res.Resources;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreference;
+import com.cherish.settings.preferences.CustomSeekBarPreference;
 import android.provider.Settings;
 import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import java.util.Locale;
+import android.text.TextUtils;
+import android.view.View;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
 
 import com.android.settings.SettingsPreferenceFragment;
+import com.cherish.settings.preferences.SystemSettingMasterSwitchPreference;
 
 public class MiscSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+    private static final String GAMING_MODE_ENABLED = "gaming_mode_enabled";
+
+    private SystemSettingMasterSwitchPreference mGamingMode;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+        ContentResolver resolver = getActivity().getContentResolver();
 
         addPreferencesFromResource(R.xml.cherish_settings_misc);
+
+        mGamingMode = (SystemSettingMasterSwitchPreference) findPreference(GAMING_MODE_ENABLED);
+        mGamingMode.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.GAMING_MODE_ENABLED, 0) == 1));
+        mGamingMode.setOnPreferenceChangeListener(this);
 
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mGamingMode) {
+        boolean value = (Boolean) objValue;
+        Settings.System.putInt(getActivity().getContentResolver(),
+                Settings.System.GAMING_MODE_ENABLED, value ? 1 : 0);
+        return true;
+        }
         return false;
     }
 
