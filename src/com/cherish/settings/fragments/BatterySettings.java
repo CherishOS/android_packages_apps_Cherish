2bef040e34e240d207b24dbad33e901443869828
ShubhamB <shubhamprince111@gmail.com>
2020-04-25 16:04:33 +0000

    BatteryBar: Let's keep only statusbar option
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>
    Change-Id: I7fc40df990bcbb3a481f3174f749c9c459d3042b
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/cherish/settings/fragments/BatterySettings.java
+++ src/com/cherish/settings/fragments/BatterySettings.java
@@ -40,7 +40,6 @@ import com.android.settings.R;
 public class BatterySettings extends SettingsPreferenceFragment
             implements Preference.OnPreferenceChangeListener  {
 
-    private static final String PREF_BATT_BAR = "statusbar_battery_bar_list";
     private static final String PREF_BATT_BAR_NO_NAVBAR = "statusbar_battery_bar_no_navbar_list";
     private static final String PREF_BATT_BAR_STYLE = "statusbar_battery_bar_style";
     private static final String PREF_BATT_BAR_COLOR = "statusbar_battery_bar_color";
@@ -61,7 +60,6 @@ public class BatterySettings extends SettingsPreferenceFragment
     //private static final int BATTERY_PERCENT_SHOW_INSIDE = 1;
     //private static final int BATTERY_PERCENT_SHOW_OUTSIDE = 2;
 
-    private ListPreference mBatteryBar;
     private ListPreference mBatteryBarNoNavbar;
     private ListPreference mBatteryBarStyle;
     private CustomSeekBarPreference mBatteryBarThickness;
@@ -87,11 +85,6 @@ public class BatterySettings extends SettingsPreferenceFragment
 
         int intColor;
         String hexColor;
-
-        mBatteryBar = (ListPreference) prefSet.findPreference(PREF_BATT_BAR);
-        mBatteryBar.setValue((Settings.System.getInt(resolver, Settings.System.STATUSBAR_BATTERY_BAR, 0)) + "");
-        mBatteryBar.setSummary(mBatteryBar.getEntry());
-        mBatteryBar.setOnPreferenceChangeListener(this);
 
         mBatteryBarNoNavbar = (ListPreference) prefSet.findPreference(PREF_BATT_BAR_NO_NAVBAR);
         mBatteryBarNoNavbar.setValue((Settings.System.getInt(resolver, Settings.System.STATUSBAR_BATTERY_BAR, 0)) + "");
@@ -155,20 +148,6 @@ public class BatterySettings extends SettingsPreferenceFragment
         mBatteryPercent.setSummary(mBatteryPercent.getEntry());
         mBatteryPercent.setOnPreferenceChangeListener(this);
 
-/*
-        boolean hasNavBarByDefault = getResources().getBoolean(
-            com.android.internal.R.bool.config_showNavigationBar);
-        boolean enableNavigationBar = Settings.Secure.getInt(resolver,
-            Settings.Secure.NAVIGATION_BAR_VISIBLE, hasNavBarByDefault ? 1 : 0) == 1;
-        boolean batteryBarSupported = Settings.Secure.getInt(resolver,
-            Settings.Secure.NAVIGATION_BAR_MODE, 0) == 0;
-        if (!enableNavigationBar || !batteryBarSupported) {
-*/
-            prefSet.removePreference(mBatteryBar);
-/*        } else {
-            prefSet.removePreference(mBatteryBarNoNavbar);
-        }*/
-
         updateBatteryBarOptions();
         mBatteryPercent.setEnabled(
                 batterystyle != BATTERY_STYLE_TEXT && batterystyle != BATTERY_STYLE_HIDDEN);
@@ -201,13 +180,6 @@ public class BatterySettings extends SettingsPreferenceFragment
             Settings.System.putInt(resolver,
                 Settings.System.STATUSBAR_BATTERY_BAR_BATTERY_LOW_COLOR, intHex);
             return true;
-        } else if (preference == mBatteryBar) {
-            int val = Integer.parseInt((String) newValue);
-            int index = mBatteryBar.findIndexOfValue((String) newValue);
-            Settings.System.putInt(resolver,
-                Settings.System.STATUSBAR_BATTERY_BAR, val);
-            mBatteryBar.setSummary(mBatteryBar.getEntries()[index]);
-            updateBatteryBarOptions();
         } else if (preference == mBatteryBarNoNavbar) {
             int val = Integer.parseInt((String) newValue);
             int index = mBatteryBarNoNavbar.findIndexOfValue((String) newValue);
