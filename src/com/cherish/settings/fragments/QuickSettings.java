69a604b199fc3b4a802a84ad7a1b279bad9cd0e6
Hưng Phan <phandinhhungvp2001@gmail.com>
2020-12-06 10:31:43 +0700

    Revert "Cherish:Add pref for qs brightness slider position [2/2]"
    This reverts commit 31e425c4daba76965b5c1d36f08ec0a127560916.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/cherish/settings/fragments/QuickSettings.java
+++ src/com/cherish/settings/fragments/QuickSettings.java
@@ -31,12 +31,10 @@ import java.util.ArrayList;
 
 public class QuickSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
-	
-   private static final String BRIGHTNESS_SLIDER = "qs_show_brightness";
+
    private static final String FOOTER_TEXT_STRING = "footer_text_string";
    private static final String STATUS_BAR_CUSTOM_HEADER = "status_bar_custom_header";
 
-    private SystemSettingMasterSwitchPreference mBrightnessSlider;
     private SystemSettingEditTextPreference mFooterString;
     private SystemSettingMasterSwitchPreference mCustomHeader;
 
@@ -48,13 +46,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements
 
         PreferenceScreen prefScreen = getPreferenceScreen();
         ContentResolver resolver = getActivity().getContentResolver();
-		
-		mBrightnessSlider = (SystemSettingMasterSwitchPreference)
-                findPreference(BRIGHTNESS_SLIDER);
-        mBrightnessSlider.setOnPreferenceChangeListener(this);
-        boolean enabled = Settings.System.getInt(resolver,
-                BRIGHTNESS_SLIDER, 1) == 1;
-        mBrightnessSlider.setChecked(enabled);
 
         mCustomHeader = (SystemSettingMasterSwitchPreference) findPreference(STATUS_BAR_CUSTOM_HEADER);
         int qsHeader = Settings.System.getInt(resolver,
@@ -78,12 +69,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-		if (preference == mBrightnessSlider) {
-            Boolean value = (Boolean) newValue;
-            Settings.System.putInt(resolver,
-                    BRIGHTNESS_SLIDER, value ? 1 : 0);
-            return true;
-        } else if (preference == mFooterString) {
+         if (preference == mFooterString) {
             String value = (String) newValue;
             if (value != "" && value != null)
                 Settings.System.putString(getActivity().getContentResolver(),
