94773f904ed4c06fa755aeb4440efbd4934f234c
Hưng Phan <phandinhhungvp2001@gmail.com>
2021-02-19 13:40:25 +0700

    Cherish:Set google sans font is default for clock and date[2/2]
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/cherish/settings/fragments/LockScreenSettings.java
+++ src/com/cherish/settings/fragments/LockScreenSettings.java
@@ -79,14 +79,14 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
         // Lockscren Clock Fonts
         mLockClockFonts = (ListPreference) findPreference(LOCK_CLOCK_FONTS);
         mLockClockFonts.setValue(String.valueOf(Settings.System.getInt(
-                getContentResolver(), Settings.System.LOCK_CLOCK_FONTS, 34)));
+                getContentResolver(), Settings.System.LOCK_CLOCK_FONTS, 28)));
         mLockClockFonts.setSummary(mLockClockFonts.getEntry());
         mLockClockFonts.setOnPreferenceChangeListener(this);
 
         // Lockscren Date Fonts
         mLockDateFonts = (ListPreference) findPreference(LOCK_DATE_FONTS);
         mLockDateFonts.setValue(String.valueOf(Settings.System.getInt(
-                getContentResolver(), Settings.System.LOCK_DATE_FONTS, 32)));
+                getContentResolver(), Settings.System.LOCK_DATE_FONTS, 28)));
         mLockDateFonts.setSummary(mLockDateFonts.getEntry());
         mLockDateFonts.setOnPreferenceChangeListener(this);
 
