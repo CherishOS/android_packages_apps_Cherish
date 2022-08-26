3f6c650139f928c6d18df287a045a3b6093be27d
spkal01 <kalligeross@gmail.com>
2022-08-26 09:53:06 +0300

    Cherish: CLock Customization
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/cherish/settings/fragments/StatusBarSettings.java
+++ src/com/cherish/settings/fragments/StatusBarSettings.java
@@ -8,8 +8,8 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.UserHandle;
 import android.content.ContentResolver;
-import android.content.Context;
 import android.content.res.Resources;
+import android.content.Context;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceGroup;
@@ -20,7 +20,6 @@ import androidx.preference.PreferenceFragment;
 import androidx.preference.SwitchPreference;
 import android.provider.Settings;
 import com.android.settings.R;
-
 import java.util.Locale;
 import android.text.TextUtils;
 import android.view.View;
@@ -31,12 +30,15 @@ import com.cherish.settings.preferences.SystemSettingSeekBarPreference;
 import com.cherish.settings.preferences.SystemSettingListPreference;
 import com.cherish.settings.preferences.SystemSettingSwitchPreference;
 import com.cherish.settings.preferences.SystemSettingMasterSwitchPreference;
+import com.cherish.settings.utils.DeviceUtils;
 import com.android.settings.Utils;
 import com.android.internal.util.cherish.CherishUtils;
 import android.util.Log;
+
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 import android.provider.SearchIndexableResource;
+import android.util.Log;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -47,12 +49,17 @@ import java.util.Collections;
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
 public class StatusBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
+
+    private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock";
+
+    private SystemSettingListPreference mStatusBarClock;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.cherish_settings_statusbar);
-		
+
 		ContentResolver resolver = getActivity().getContentResolver();
 
         PreferenceScreen prefSet = getPreferenceScreen();
@@ -63,19 +70,17 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
         ContentResolver resolver = getActivity().getContentResolver();
         return false;
     }
-	
+
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.CHERISH_SETTINGS;
     }
-	
+
 	/**
      * For Search.
      */
-
     public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
             new BaseSearchIndexProvider() {
-
                 @Override
                 public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
                         boolean enabled) {
@@ -86,12 +91,10 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
                     result.add(sir);
                     return result;
                 }
-
                 @Override
                 public List<String> getNonIndexableKeys(Context context) {
                     List<String> keys = super.getNonIndexableKeys(context);
                     return keys;
                 }
     };
-
 }
