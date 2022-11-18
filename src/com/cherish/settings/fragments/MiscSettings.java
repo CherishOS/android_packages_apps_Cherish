b2fe86fb80e28ff224f3c238fe445ed33b1e2601
minaripenguin <minaripenguin@users.noreply.github.com>
2022-11-18 16:18:25 +0800

    Cherish: Allow toggling netflix spoofing
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/cherish/settings/fragments/MiscSettings.java
+++ src/com/cherish/settings/fragments/MiscSettings.java
@@ -38,6 +38,7 @@ import com.cherish.settings.preferences.SystemSettingMasterSwitchPreference;
 import com.cherish.settings.preferences.SystemSettingListPreference;
 import com.cherish.settings.preferences.SecureSettingSwitchPreference;
 import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.development.SystemPropPoker;
 import com.android.settingslib.search.SearchIndexable;
 import android.provider.SearchIndexableResource;
 
@@ -50,15 +51,18 @@ public class MiscSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 	private static final String KEY_GAMES_SPOOF = "use_games_spoof";
     private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
+    private static final String KEY_NETFLIX_SPOOF = "use_netflix_spoof";
 
     private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
     private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
-	
+	private static final String SYS_NETFLIX_SPOOF = "persist.sys.spoof_netflix";
+
 	private static final String SMART_CHARGING = "smart_charging";
 
     private SwitchPreference mGamesSpoof;
     private SwitchPreference mPhotosSpoof;
 	private Preference mSmartCharging;
+    private SwitchPreference mNetFlixSpoof;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -77,6 +81,10 @@ public class MiscSettings extends SettingsPreferenceFragment implements
 		mPhotosSpoof = (SwitchPreference) findPreference(KEY_PHOTOS_SPOOF);
         mPhotosSpoof.setChecked(SystemProperties.getBoolean(SYS_PHOTOS_SPOOF, true));
         mPhotosSpoof.setOnPreferenceChangeListener(this);
+
+        mNetFlixSpoof = (SwitchPreference) findPreference(KEY_NETFLIX_SPOOF);
+        mNetFlixSpoof.setChecked(SystemProperties.getBoolean(SYS_NETFLIX_SPOOF, false));
+        mNetFlixSpoof.setOnPreferenceChangeListener(this);
 		
 		mSmartCharging = (Preference) prefScreen.findPreference(SMART_CHARGING);
         boolean mSmartChargingSupported = res.getBoolean(
@@ -91,10 +99,17 @@ public class MiscSettings extends SettingsPreferenceFragment implements
         if (preference == mGamesSpoof) {
             boolean value = (Boolean) newValue;
             SystemProperties.set(SYS_GAMES_SPOOF, value ? "true" : "false");
+            SystemPropPoker.getInstance().poke();
             return true;
 		} else if (preference == mPhotosSpoof) {
             boolean value = (Boolean) newValue;
             SystemProperties.set(SYS_PHOTOS_SPOOF, value ? "true" : "false");
+            SystemPropPoker.getInstance().poke();
+            return true;
+        } else if (preference == mNetFlixSpoof) {
+            boolean value = (Boolean) newValue;
+            SystemProperties.set(SYS_NETFLIX_SPOOF, value ? "true" : "false");
+            SystemPropPoker.getInstance().poke();
             return true;
 		}
         return false;
