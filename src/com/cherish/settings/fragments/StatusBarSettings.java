1ecee0b4c16dde5889d20c1e7142b906beb5605a
Hưng Phan <phandinhhungvp2001@gmail.com>
2023-01-10 20:03:05 +0700

    Revert "Cherish: Make combined signal icons configurable at runtime [2/2]"
    This reverts commit 2581ff49a187d30ae7b78e3ecf867d6fc2aa4fba.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/cherish/settings/fragments/StatusBarSettings.java
+++ src/com/cherish/settings/fragments/StatusBarSettings.java
@@ -20,29 +20,23 @@ import androidx.preference.PreferenceFragment;
 import androidx.preference.SwitchPreference;
 import android.provider.Settings;
 import com.android.settings.R;
-import android.content.DialogInterface;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.content.res.Resources;
+
 import java.util.Locale;
-import android.os.SystemProperties;
 import android.text.TextUtils;
 import android.view.View;
-import com.cherish.settings.preferences.SystemSettingSwitchPreference;
-import com.cherish.settings.preferences.SystemSettingSeekBarPreference;
-import com.cherish.settings.preferences.SecureSettingSwitchPreference;
-import com.cherish.settings.preferences.SystemSettingListPreference;
+
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settingslib.development.SystemPropPoker;
+import com.cherish.settings.preferences.CustomSeekBarPreference;
 import com.cherish.settings.preferences.SystemSettingSeekBarPreference;
+import com.cherish.settings.preferences.SystemSettingListPreference;
+import com.cherish.settings.preferences.SystemSettingSwitchPreference;
+import com.cherish.settings.preferences.SystemSettingMasterSwitchPreference;
 import com.android.settings.Utils;
 import com.android.internal.util.cherish.CherishUtils;
 import android.util.Log;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 import android.provider.SearchIndexableResource;
-import com.android.settings.SettingsPreferenceFragment;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -53,11 +47,6 @@ import java.util.Collections;
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
 public class StatusBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
-			
-	private static final String KEY_COMBINED_SIGNAL_ICONS = "enable_combined_signal_icons";
-    private static final String SYS_COMBINED_SIGNAL_ICONS = "persist.sys.enable.combined_signal_icons";
-	
-	private SwitchPreference mCombinedSignalIcons;
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -67,23 +56,11 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
 		ContentResolver resolver = getActivity().getContentResolver();
 
         PreferenceScreen prefSet = getPreferenceScreen();
-		
-		mCombinedSignalIcons = (SwitchPreference) findPreference(KEY_COMBINED_SIGNAL_ICONS);
-        mCombinedSignalIcons.setChecked(SystemProperties.getBoolean(SYS_COMBINED_SIGNAL_ICONS, false));
-        mCombinedSignalIcons.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-		 if (preference == mCombinedSignalIcons) {
-            boolean value = (Boolean) objValue;
-            Settings.Secure.putIntForUser(getContentResolver(),
-                Settings.Secure.ENABLE_COMBINED_SIGNAL_ICONS, value ? 1 : 0, UserHandle.USER_CURRENT);
-            SystemProperties.set(SYS_COMBINED_SIGNAL_ICONS, value ? "true" : "false");
-            SystemPropPoker.getInstance().poke();
-            return true;
-		}
         return false;
     }
 	
