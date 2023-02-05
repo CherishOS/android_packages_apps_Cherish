266754500325b6b56a612b1d08ba822d0b39cd15
rdx420 <padraramesh420@gmail.com>
2023-02-05 11:49:27 +0530

    Cherish:Add 6 new QS Styles
    Signed-off-by: rdx420 <padraramesh420@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/cherish/settings/fragments/ThemeSettings.java
+++ src/com/cherish/settings/fragments/ThemeSettings.java
@@ -201,6 +201,18 @@ public class ThemeSettings extends DashboardFragment implements OnPreferenceChan
             setQsStyle(mOverlayService, "com.android.system.qs.outline");
         } else if (qsPanelStyle == 2 || qsPanelStyle == 3) {
             setQsStyle(mOverlayService, "com.android.system.qs.twotoneaccent");
+		} else if (qsPanelStyle == 4) {
+            setQsStyle(mOverlayService, "com.android.system.qs.shaded");
+        } else if (qsPanelStyle == 5) {
+            setQsStyle(mOverlayService, "com.android.system.qs.cyberpunk");
+        } else if (qsPanelStyle == 6) {
+            setQsStyle(mOverlayService, "com.android.system.qs.neumorph");
+        } else if (qsPanelStyle == 7) {
+            setQsStyle(mOverlayService, "com.android.system.qs.reflected");
+        } else if (qsPanelStyle == 8) {
+            setQsStyle(mOverlayService, "com.android.system.qs.surround");
+        } else if (qsPanelStyle == 9) {
+            setQsStyle(mOverlayService, "com.android.system.qs.thin");
         }
     }
 
@@ -233,7 +245,13 @@ public class ThemeSettings extends DashboardFragment implements OnPreferenceChan
 
     public static final String[] QS_STYLES = {
         "com.android.system.qs.outline",
-        "com.android.system.qs.twotoneaccent"
+        "com.android.system.qs.twotoneaccent",
+        "com.android.system.qs.shaded",
+        "com.android.system.qs.cyberpunk",
+        "com.android.system.qs.neumorph",
+        "com.android.system.qs.reflected",
+        "com.android.system.qs.surround",
+        "com.android.system.qs.thin"
     };
 
     @Override
