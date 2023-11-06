15718bc0ad045694db9bd95fbee36b245767cf0b
SuperDroidBond <SuperDroidBond@yahoo.com>
2023-11-06 13:21:44 +0700

    [SQUASHED] SystemUI: user selectable VoLTE/VoWifi Icons [2/2]
    This reverts commit dc7f7b2ee508a56be321e1e25f29e6a710e104df.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/cherish/settings/fragments/StatusBarSettings.java
+++ src/com/cherish/settings/fragments/StatusBarSettings.java
@@ -46,7 +46,14 @@ import java.util.Collections;
 
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
 public class StatusBarSettings extends SettingsPreferenceFragment implements
-        OnPreferenceChangeListener {
+    Preference.OnPreferenceChangeListener {
+
+    private static final String VOLTE_ICON_STYLE = "volte_icon_style";
+    private static final String VOWIFI_ICON_STYLE = "vowifi_icon_style";
+    
+    private SystemSettingListPreference mVolteIconStyle;
+    private SystemSettingListPreference mVowifiIconStyle;
+    
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -56,11 +63,35 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
 		ContentResolver resolver = getActivity().getContentResolver();
 
         PreferenceScreen prefSet = getPreferenceScreen();
+        mVowifiIconStyle = (SystemSettingListPreference) findPreference(VOWIFI_ICON_STYLE);
+        mVolteIconStyle = (SystemSettingListPreference) findPreference(VOLTE_ICON_STYLE);
+
+        int vowifiIconStyle = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.VOWIFI_ICON_STYLE, 1);
+        mVowifiIconStyle.setValue(String.valueOf(vowifiIconStyle));
+        mVowifiIconStyle.setOnPreferenceChangeListener(this);
+        if (vowifiIconStyle == 0) {
+            mVolteIconStyle.setEnabled(true);
+        } else {
+            mVolteIconStyle.setEnabled(false);
+        }
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mVowifiIconStyle) {
+            int vowifiIconStyle = Integer.parseInt(((String) newValue).toString());
+            Settings.System.putInt(resolver,
+                  Settings.System.VOWIFI_ICON_STYLE, vowifiIconStyle);
+            mVowifiIconStyle.setValue(String.valueOf(vowifiIconStyle));
+            if (vowifiIconStyle == 0) {
+                mVolteIconStyle.setEnabled(true);
+            } else {
+                mVolteIconStyle.setEnabled(false);
+            }
+            return true;
+        }
         return false;
     }
 	
