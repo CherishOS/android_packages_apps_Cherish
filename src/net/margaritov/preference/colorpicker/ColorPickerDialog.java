fb5bc12bda114116d30bdde945dbfbc5b6c4a6c3
Hưng Phan <phandinhhungvp2001@gmail.com>
2023-11-12 14:00:50 +0700

    Revert "Color picker: allow to activate ongoing led light as color preview"
    This reverts commit 608519fb3af9def57c320c4dacd58896b653eea5.

--- src/net/margaritov/preference/colorpicker/ColorPickerDialog.java
+++ src/net/margaritov/preference/colorpicker/ColorPickerDialog.java
@@ -18,7 +18,6 @@
 package net.margaritov.preference.colorpicker;
 
 import android.app.AlertDialog;
-import android.app.NotificationManager;
 import android.content.Context;
 import android.graphics.PixelFormat;
 import android.os.Bundle;
@@ -40,21 +39,15 @@ public class ColorPickerDialog extends AlertDialog implements ColorPickerView.On
     private ColorPickerPanelView mNewColor;
     private EditText mHex;
 
-    private boolean mShowLedPreview;
-
-    private NotificationManager mNoMan;
-    private Context mContext;
-
     private OnColorChangedListener mListener;
 
     public interface OnColorChangedListener {
         void onColorChanged(int color);
     }
 
-    ColorPickerDialog(Context context, int initialColor, boolean showLedPreview) {
+    ColorPickerDialog(Context context, int initialColor) {
         super(context);
-        mContext = context;
-        mShowLedPreview = showLedPreview;
+
         init(initialColor);
     }
 
@@ -79,8 +72,6 @@ public class ColorPickerDialog extends AlertDialog implements ColorPickerView.On
 
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(
                 Context.LAYOUT_INFLATER_SERVICE);
-        mNoMan = (NotificationManager)
-                mContext.getSystemService(Context.NOTIFICATION_SERVICE);
 
         assert inflater != null;
         View layout = inflater.inflate(R.layout.dui_dialog_color_picker, null);
@@ -99,7 +90,6 @@ public class ColorPickerDialog extends AlertDialog implements ColorPickerView.On
         mColorPicker.setOnColorChangedListener(this);
         mOldColor.setColor(color);
         mColorPicker.setColor(color, true);
-        showLed(color);
 
         if (mHex != null) {
             mHex.setText(ColorPickerPreference.convertToRGB(color));
@@ -118,23 +108,6 @@ public class ColorPickerDialog extends AlertDialog implements ColorPickerView.On
                 mHex.setText(ColorPickerPreference.convertToRGB(color));
             }
         } catch (Exception ignored) {
-        }
-        showLed(color);
-    }
-
-    private void showLed(int color) {
-        if (mShowLedPreview) {
-            if (color == 0xFFFFFFFF) {
-                // argb white doesn't work
-                color = 0xffffff;
-            }
-            mNoMan.forceShowLedLight(color);
-        }
-    }
-
-    private void switchOffLed() {
-        if (mShowLedPreview) {
-            mNoMan.forceShowLedLight(-1);
         }
     }
 
@@ -173,15 +146,7 @@ public class ColorPickerDialog extends AlertDialog implements ColorPickerView.On
             }
         }
         dismiss();
-        switchOffLed();
     }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        switchOffLed();
-    }
-
 
     @NonNull
     @Override
@@ -190,7 +155,6 @@ public class ColorPickerDialog extends AlertDialog implements ColorPickerView.On
         state.putInt("old_color", mOldColor.getColor());
         state.putInt("new_color", mNewColor.getColor());
         dismiss();
-        switchOffLed();
         return state;
     }
 
