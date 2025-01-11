1d0d1c450514057858c5bd93e9d09a01338f7bb1
Dmitrii <bankersenator@gmail.com>
2025-01-11 03:08:20 -0500

    Cherish: migrate monet settings to switchpreferencecompat
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/cherish/settings/fragments/MonetSettings.java
+++ src/com/cherish/settings/fragments/MonetSettings.java
@@ -24,7 +24,7 @@ import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.SwitchPreference;
+import androidx.preference.SwitchPreferenceCompat;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.dashboard.DashboardFragment;
@@ -78,11 +78,11 @@ public class MonetSettings extends DashboardFragment implements
 
     private ListPreference mColorSourcePref;
     private ColorPickerPreference mAccentColorPref;
-    private SwitchPreference mAccentBackgroundPref;
+    private SwitchPreferenceCompat mAccentBackgroundPref;
     private ColorPickerPreference mBgColorPref;
     private CustomSeekBarPreference mLuminancePref;
     private CustomSeekBarPreference mChromaPref;
-    private SwitchPreference mTintBackgroundPref;
+    private SwitchPreferenceCompat mTintBackgroundPref;
     private CustomSeekBarPreference mShadeBlurRadiusPref;
     private SwitchPreferenceCompat mNotificationRowTransparencyPref;
     private SwitchPreferenceCompat mDualToneShadePref;
