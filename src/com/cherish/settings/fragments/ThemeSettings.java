0428e50e35e76f9912c930e137831c0211513bda
someone5678 <someone5678@users.noreply.github.com>
2025-09-05 13:01:48 +0700

    Cherish: Allow to disable Pixel Launcher theme icons

--- src/com/cherish/settings/fragments/ThemeSettings.java
+++ src/com/cherish/settings/fragments/ThemeSettings.java
@@ -45,17 +45,15 @@ import android.view.MenuItem;
 import android.view.MenuInflater;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import com.android.settingslib.core.AbstractPreferenceController;
-import com.android.settings.SettingsPreferenceFragment;
-import com.android.settingslib.core.AbstractPreferenceController;
-import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settings.development.OverlayCategoryPreferenceController;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.core.lifecycle.Lifecycle;
 import java.util.Locale;
 import android.text.TextUtils;
 import android.view.View;
-import android.provider.SearchIndexableResource;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Objects;
@@ -68,6 +66,8 @@ import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
 public class ThemeSettings extends DashboardFragment implements OnPreferenceChangeListener {
+
+    private static final String KEY_LAUNCHER_CATEGORY = "themes_launcher_category";
 			
 	public static final String TAG = "ThemeSettings";
     static final int DEFAULT_QS_PANEL_COLOR = 0xffffffff;
@@ -76,6 +76,7 @@ public class ThemeSettings extends DashboardFragment implements OnPreferenceChan
 
     private IOverlayManager mOverlayService;
     private UiModeManager mUiModeManager;
+    private PreferenceCategory mLauncherCategory;
 	
 	@Override
     protected String getLogTag() {
@@ -107,6 +108,12 @@ public class ThemeSettings extends DashboardFragment implements OnPreferenceChan
         ContentResolver resolver = getActivity().getContentResolver();
 		final Resources res = getResources();
 		mContext =  getActivity();
+
+         mLauncherCategory = (PreferenceCategory) findPreference(KEY_LAUNCHER_CATEGORY);
+
+         if (!CherishUtils.isPackageInstalled(context, "com.google.android.apps.nexuslauncher")) {
+            prefScreen.removePreference(mLauncherCategory);
+        }
         }
 
     public boolean isAvailable() {
@@ -128,24 +135,18 @@ public class ThemeSettings extends DashboardFragment implements OnPreferenceChan
      * For Search.
      */
 
-    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider() {
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+        new BaseSearchIndexProvider(R.xml.cherish_settings_themes) {
 
-                @Override
-                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
-                        boolean enabled) {
-                    ArrayList<SearchIndexableResource> result =
-                            new ArrayList<SearchIndexableResource>();
-                    SearchIndexableResource sir = new SearchIndexableResource(context);
-                    sir.xmlResId = R.xml.cherish_settings_theme;
-                    result.add(sir);
-                    return result;
-                }
+            @Override
+            public List<String> getNonIndexableKeys(Context context) {
+                List<String> keys = super.getNonIndexableKeys(context);
+                final Resources resources = context.getResources();
 
-                @Override
-                public List<String> getNonIndexableKeys(Context context) {
-                    List<String> keys = super.getNonIndexableKeys(context);
-                    return keys;
+                if (!CherishUtils.isPackageInstalled(context, "com.google.android.apps.nexuslauncher")) {
+                    keys.add(KEY_LAUNCHER_CATEGORY);
                 }
-    };
+                return keys;
+            }
+        };
 }
