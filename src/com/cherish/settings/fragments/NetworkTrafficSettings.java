5030f94be40c19f6e35fc3e825814bdba0fc2e3e
Pranav Vashi <neobuddy89@gmail.com>
2025-10-04 23:37:05 +0700

    Cherish: Clean up NetworkTraffic
    Signed-off-by: hungphan2001 <phandinhhungvp2001@gmail.com>

--- src/com/cherish/settings/fragments/NetworkTrafficSettings.java
+++ src/com/cherish/settings/fragments/NetworkTrafficSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-2021 crDroid Android Project
+ * Copyright (C) 2017-2024 crDroid Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,100 +20,41 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.UserHandle;
-import android.provider.Settings;
 
-import androidx.preference.ListPreference;
-import androidx.preference.Preference;
-import androidx.preference.PreferenceScreen;
-import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.SwitchPreferenceCompat;
-
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.internal.logging.nano.MetricsProto;
+import com.android.settingslib.search.SearchIndexable;
 
-import com.cherish.settings.preferences.CustomSeekBarPreference;
+import androidx.preference.Preference;
 
-public class NetworkTrafficSettings extends SettingsPreferenceFragment
-        implements Preference.OnPreferenceChangeListener  {
+import java.util.List;
+
+@SearchIndexable
+public class NetworkTrafficSettings extends SettingsPreferenceFragment {
 
     private static final String TAG = "NetworkTrafficSettings";
-
-    private CustomSeekBarPreference mNetTrafficAutohideThreshold;
-    private CustomSeekBarPreference mNetTrafficRefreshInterval;
-    private ListPreference mNetTrafficLocation;
-    private ListPreference mNetTrafficMode;
-    private ListPreference mNetTrafficUnits;
-    private SwitchPreferenceCompat mNetTrafficAutohide;
-    private SwitchPreferenceCompat mNetTrafficHideArrow;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         addPreferencesFromResource(R.xml.network_traffic_settings);
-        final ContentResolver resolver = getActivity().getContentResolver();
-
-        mNetTrafficAutohideThreshold = (CustomSeekBarPreference)
-                findPreference(Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD);
-        mNetTrafficRefreshInterval = (CustomSeekBarPreference)
-                findPreference(Settings.System.NETWORK_TRAFFIC_REFRESH_INTERVAL);
-        mNetTrafficLocation = (ListPreference)
-                findPreference(Settings.System.NETWORK_TRAFFIC_LOCATION);
-        mNetTrafficLocation.setOnPreferenceChangeListener(this);
-        mNetTrafficMode = (ListPreference)
-                findPreference(Settings.System.NETWORK_TRAFFIC_MODE);
-        mNetTrafficAutohide = (SwitchPreferenceCompat)
-                findPreference(Settings.System.NETWORK_TRAFFIC_AUTOHIDE);
-        mNetTrafficUnits = (ListPreference)
-                findPreference(Settings.System.NETWORK_TRAFFIC_UNITS);
-        mNetTrafficHideArrow = (SwitchPreferenceCompat)
-                findPreference(Settings.System.NETWORK_TRAFFIC_HIDEARROW);
-
-        int location = Settings.System.getIntForUser(resolver,
-                Settings.System.NETWORK_TRAFFIC_LOCATION, 0, UserHandle.USER_CURRENT);
-        updateEnabledStates(location);
-    }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mNetTrafficLocation) {
-            int location = Integer.valueOf((String) newValue);
-            updateEnabledStates(location);
-            return true;
-        }
-        return false;
-    }
-
-    private void updateEnabledStates(int location) {
-        final boolean enabled = location != 0;
-        mNetTrafficMode.setEnabled(enabled);
-        mNetTrafficAutohide.setEnabled(enabled);
-        mNetTrafficAutohideThreshold.setEnabled(enabled);
-        mNetTrafficHideArrow.setEnabled(enabled);
-        mNetTrafficRefreshInterval.setEnabled(enabled);
-        mNetTrafficUnits.setEnabled(enabled);
-    }
-
-    public static void reset(Context mContext) {
-        ContentResolver resolver = mContext.getContentResolver();
-        Settings.System.putIntForUser(resolver,
-                Settings.System.NETWORK_TRAFFIC_LOCATION, 0, UserHandle.USER_CURRENT);
-        Settings.System.putIntForUser(resolver,
-                Settings.System.NETWORK_TRAFFIC_MODE, 0, UserHandle.USER_CURRENT);
-        Settings.System.putIntForUser(resolver,
-                Settings.System.NETWORK_TRAFFIC_AUTOHIDE, 1, UserHandle.USER_CURRENT);
-        Settings.System.putIntForUser(resolver,
-                Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, 0, UserHandle.USER_CURRENT);
-        Settings.System.putIntForUser(resolver,
-                Settings.System.NETWORK_TRAFFIC_UNITS, 1, UserHandle.USER_CURRENT);
-        Settings.System.putIntForUser(resolver,
-                Settings.System.NETWORK_TRAFFIC_REFRESH_INTERVAL, 2, UserHandle.USER_CURRENT);
-        Settings.System.putIntForUser(resolver,
-                Settings.System.NETWORK_TRAFFIC_HIDEARROW, 0, UserHandle.USER_CURRENT);
     }
 
     @Override
     public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.CHERISH_SETTINGS;
+        return MetricsEvent.CHERISH_SETTINGS;
     }
+
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+        new BaseSearchIndexProvider(R.xml.network_traffic_settings) {
+
+            @Override
+            public List<String> getNonIndexableKeys(Context context) {
+                List<String> keys = super.getNonIndexableKeys(context);
+                return keys;
+            }
+        };
 }
